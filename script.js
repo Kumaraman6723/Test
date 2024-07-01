af05b36441b75414c4a0f3cd36ed23fce0e9bbe9
Aman kumar prasad <kumarprasadaman1234@gmail.com>
2024-07-01 12:24:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -603,9 +603,7 @@ document
 document.getElementById("tickets-link").addEventListener("click", function () {
   showContent("tickets-content");
 });
-document.getElementById("users-link").addEventListener("click", function () {
-  showContent("users-content");
-});
+
 document.getElementById("settings-link").addEventListener("click", function () {
   showContent("settings-content");
 });
@@ -618,3 +616,44 @@ document.getElementById("clickable-div").addEventListener("click", function () {
   displayTextDiv.style.display = "block";
   displayTextDiv.innerHTML = "<p>You clicked on the left-side div!</p>";
 });
+var xmlhttp = new XMLHttpRequest();
+var url = "http://127.0.0.1:5500/jsonData.json";
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var data = JSON.parse(this.responseText);
+
+    var dates = data.date_population.map(function (elem) {
+      return elem.date;
+    });
+    var populations = data.date_population.map(function (elem) {
+      return elem.population;
+    });
+
+    const ctx = document.getElementById("canvas").getContext("2d");
+    const myChart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: dates,
+        datasets: [
+          {
+            label: "Population",
+            data: populations,
+            backgroundColor: "rgba(255, 51, 94, 0.2)",
+            borderColor: "#ff335e",
+            fill: true,
+            tension: 0.4,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }
+};
